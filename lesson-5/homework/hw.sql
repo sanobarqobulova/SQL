3b6226cc7263289c8aecbe78b07ffbda77a80fe8
Sanobar <sanobarkobulova@gmail.com>
2025-05-14 23:09:47 +0500

    Update hw.sql

--- lesson-5/homework/hw.sql
+++ lesson-5/homework/hw.sql
@@ -267,8 +267,83 @@ INSERT INTO Orders VALUES
 (39, 39, 40, '2023-11-26', 3, 120.00),
 (40, 40, 1, '2024-03-09', 1, 1200.00);
 
+--Easy-Level Tasks
+--1.Write a query that uses an alias to rename the ProductName column as Name in the Products table.
 
---Write a query that uses CASE to assign 'Top Tier' if SaleAmount > 500, 'Mid Tier' if SaleAmount BETWEEN 200 AND 500, and 'Low Tier'
+SELECT ProductName AS Name
+FROM Products;
+
+--2.Write a query that uses an alias to rename the Customers table as Client for easier reference.
+
+SELECT * FROM Customers AS Client;
+
+--3.Use UNION to combine results from two queries that select ProductName from Products and ProductName from Products_Discounted.
+
+SELECT ProductName FROM Products
+UNION
+SELECT ProductName FROM Products_Discounted
+ORDER BY ProductName;
+
+--4.Write a query to find the intersection of Products and Products_Discounted tables using INTERSECT.
+
+SELECT *  
+FROM Products, Products_Discounted
+INTERSECT
+SELECT * 
+FROM Products,Products_Discounted
+
+--5.Write a query to select distinct customer names and their corresponding Country using SELECT DISTINCT.
+
+select distinct FirstName, LastName, Country from Customers;
+select * from Customers
+
+--6.Write a query that uses CASE to create a conditional column that displays 'High' if Price > 1000, and 'Low' if Price <= 1000 from Products table.
+
+select Price,
+case when Price>1000 then 'High'
+when Price<=1000 then 'Low' 
+end as 'Values'
+from Products;
+
+
+--7.Use IIF to create a column that shows 'Yes' if Stock > 100, and 'No' otherwise (Products_Discounted table, StockQuantity column).
+
+select iif(StockQuantity>100, 'yes', 'no')
+from Products_Discounted;
+
+--Medium-Level Tasks
+--8.Use UNION to combine results from two queries that select ProductName from Products and ProductName from Products_Discounted tables.
+ 
+SELECT ProductName FROM Products
+UNION
+SELECT ProductName FROM Products_Discounted
+ORDER BY ProductName;
+
+--9.Write a query that returns the difference between the Products and Products_Discounted tables using EXCEPT.
+
+select * from Products
+EXCEPT
+select * from Products_Discounted
+
+--10.Create a conditional column using IIF that shows 'Expensive' if the Price is greater than 1000, and 'Affordable' if less, from Products table.
+
+select iif(Price>1000, 'Expensive', 'Affordable') from Products;
+
+--11.Write a query to find employees in the Employees table who have either Age < 25 or Salary > 60000.
+
+select  Age, salary
+from Employees
+where Age<25 or Salary>60000;
+
+
+--12.Update the salary of an employee based on their department, increase by 10% if they work in 'HR' or EmployeeID = 5
+
+update Employees
+set Salary = Salary * 1.10
+where DepartmentName='HR' or EmployeeID=5
+
+
+--13.Write a query that uses CASE to assign 'Top Tier' if SaleAmount > 500, 'Mid Tier' if SaleAmount BETWEEN 200 AND 500, and 'Low Tier'
 --otherwise. (From Sales table)
 select *, 
 CASE WHEN SaleAmount>500 THEN 'Top Tier'
@@ -285,7 +360,7 @@ except
 select CustomerID
 from Customers
 
---Write a query that uses a CASE statement to determine the discount percentage based on the quantity purchased. 
+--15.Write a query that uses a CASE statement to determine the discount percentage based on the quantity purchased. 
 --Use orders table. Result set should show customerid, quantity and discount percentage. 
 --The discount should be applied as follows: 1 item: 3% Between 1 and 3 items : 5% Otherwise: 7%
 
